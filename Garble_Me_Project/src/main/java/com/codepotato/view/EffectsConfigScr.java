5f2c44e405c6d2cd618b86236cc3022128d184b5
kungfulucky7 <terrywong283@live.com>
2014-04-16 17:41:19 -0700

    Implemented initial RecordingLibraScr!

--- Garble_Me_Project/src/main/java/com/codepotato/view/EffectsConfigScr.java
+++ Garble_Me_Project/src/main/java/com/codepotato/view/EffectsConfigScr.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class EffectsConfigScr extends Activity {
@@ -14,6 +16,14 @@ public class EffectsConfigScr extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_effects_config_scr);
         getActionBar().setDisplayHomeAsUpEnabled(true);
+        Button add = (Button) findViewById(R.id.addButton);
+        add.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Intent intent = new Intent(EffectsConfigScr.this, EffectSettingsScr.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
