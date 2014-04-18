4840d7ae143a046601dc1aaaf90f93d649a2a52c
David D. Hua <dhd12391@gmail.com>
2014-04-18 12:47:20 -0700

    worked on audiocontroller (importing player and effectchain's functions)

--- Garble_Me_Project/src/main/java/com/codepotato/controller/AudioController.java
+++ Garble_Me_Project/src/main/java/com/codepotato/controller/AudioController.java
@@ -3,6 +3,9 @@ package com.codepotato.controller;
 import com.codepotato.model.*;
 import com.codepotato.model.effects.*;
 
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Created by David on 4/16/2014.
  */
@@ -10,25 +13,34 @@ public class AudioController {
     private Player audioPlayer;
     private EffectChain effectChain;
 
-    public AudioController(){
-    //    audioPlayer = new Player ();
+    public AudioController(File filename){
+        try {
+            audioPlayer = new Player(filename);
+        }catch(Exception IOException){
+            System.out.println("ERROR: File does not exist!");
+        }
+//        effectChain = new EffectChain(filename);  //TO DO: MAKE EFFECTCHAINFACTORY FOR SINGLETON OBJECT SHARING
     }
-/*
-    public void addEffect(Effect eff){
 
+    public void addEffect(Effect eff){
+        effectChain.addEffect(eff);
+    }
+
+    public void removeEffect(int effID){
+        effectChain.removeEffect(effID);
     }
 
     public void play(){
-
+        audioPlayer.play();
     }
 
     public void pause(){
-
+        audioPlayer.pause();;
     }
 
     public boolean isPlaying(){
-
+        return audioPlayer.isPlaying();
     }
-*/
+
 
 }
