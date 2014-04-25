4ba931b44935a3e65c7c71d603bc38526c72c493
kungfulucky7 <terrywong283@live.com>
2014-04-25 01:03:13 -0700

    :q alksjdf# Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.

--- Garble_Me_Project/src/main/java/com/codepotato/view/echoFragment.java
+++ Garble_Me_Project/src/main/java/com/codepotato/view/echoFragment.java
@@ -1,7 +1,19 @@
 package com.codepotato.view;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by terrywong on 4/24/14.
  */
-public class echoFragment {
+public class EchoFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.echo_fragment, container, false);
+    }
 }
