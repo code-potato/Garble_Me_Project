03b8ded4f45331d27b04c023757cefe614a387ca
kungfulucky7 <terrywong283@live.com>
2014-04-25 17:32:50 -0700

    Just do it

--- Garble_Me_Project/src/main/java/com/codepotato/view/echoFragment.java
+++ Garble_Me_Project/src/main/java/com/codepotato/view/echoFragment.java
@@ -2,9 +2,12 @@ package com.codepotato.view;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 /**
  * Created by terrywong on 4/24/14.
@@ -14,6 +17,28 @@ public class EchoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.echo_fragment, container, false);
+        View view = inflater.inflate(R.layout.echo_fragment, container, false);
+        // SeekBar for the delayBar
+        SeekBar delayBar = (SeekBar) view.findViewById(R.id.delayBar);
+        if (delayBar != null)
+            Log.d(InitialScr.LOG_TAG, "Delay bar is found!");
+        delayBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressChanged = 0;
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChanged = progress;
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Log.d(InitialScr.LOG_TAG, "Delay bar is touched!");
+                Toast.makeText(getActivity().getBaseContext(), "Delay Bar Progress: " + progressChanged, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
     }
 }
