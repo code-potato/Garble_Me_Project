9239de2d1d2a2ace87d42aef802a67ce93f0daf3
sporthand <sporthand@namver.com>
2014-04-28 00:22:06 -0700

    update EchoFragment and ChorusFragment

--- Garble_Me_Project/src/main/java/com/codepotato/view/ChorusFragment.java
+++ Garble_Me_Project/src/main/java/com/codepotato/view/ChorusFragment.java
@@ -18,7 +18,67 @@ public class ChorusFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.chorus_fragment, container, false);
-        // SeekBar for the dryBar
+        // SeekBar for the rateBar
+        SeekBar rateBar = (SeekBar) view.findViewById(R.id.rateBar);
+        if (rateBar != null)
+            Log.d(InitialScr.LOG_TAG, "Rate bar is found!");
+        rateBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressChanged = 0;
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChanged = progress;
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Log.d(InitialScr.LOG_TAG, "Rate bar is touched!");
+                Toast.makeText(getActivity().getBaseContext(), "Rate Bar Progress: " + progressChanged, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        SeekBar depthBar = (SeekBar) view.findViewById(R.id.depthBar);
+        if (depthBar != null)
+            Log.d(InitialScr.LOG_TAG, "Depth bar is found!");
+        depthBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressChanged = 0;
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChanged = progress;
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Log.d(InitialScr.LOG_TAG, "Depth bar is touched!");
+                Toast.makeText(getActivity().getBaseContext(), "Depth Bar Progress: " + progressChanged, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        SeekBar wetBar = (SeekBar) view.findViewById(R.id.wetBar);
+        if (wetBar != null)
+            Log.d(InitialScr.LOG_TAG, "Wet bar is found!");
+        wetBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressChanged = 0;
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChanged = progress;
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Log.d(InitialScr.LOG_TAG, "Wet bar is touched!");
+                Toast.makeText(getActivity().getBaseContext(), "Wet Bar Progress: " + progressChanged, Toast.LENGTH_SHORT).show();
+            }
+        });
+
         SeekBar dryBar = (SeekBar) view.findViewById(R.id.dryBar);
         if (dryBar != null)
             Log.d(InitialScr.LOG_TAG, "Dry bar is found!");
@@ -36,6 +96,26 @@ public class ChorusFragment extends Fragment {
             public void onStopTrackingTouch(SeekBar seekBar) {
                 Log.d(InitialScr.LOG_TAG, "Dry bar is touched!");
                 Toast.makeText(getActivity().getBaseContext(), "Dry Bar Progress: " + progressChanged, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        SeekBar feedbackBar = (SeekBar) view.findViewById(R.id.feedbackBar);
+        if (feedbackBar != null)
+            Log.d(InitialScr.LOG_TAG, "Feedback bar is found!");
+        feedbackBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressChanged = 0;
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChanged = progress;
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Log.d(InitialScr.LOG_TAG, "Feedback bar is touched!");
+                Toast.makeText(getActivity().getBaseContext(), "Feedback Bar Progress: " + progressChanged, Toast.LENGTH_SHORT).show();
             }
         });
 
--- Garble_Me_Project/src/main/java/com/codepotato/view/EchoFragment.java
+++ Garble_Me_Project/src/main/java/com/codepotato/view/EchoFragment.java
@@ -38,6 +38,66 @@ public class EchoFragment extends Fragment {
                 Toast.makeText(getActivity().getBaseContext(), "Delay Bar Progress: " + progressChanged, Toast.LENGTH_SHORT).show();
             }
         });
+        SeekBar wetBar = (SeekBar) view.findViewById(R.id.wetBar);
+        if (wetBar != null)
+            Log.d(InitialScr.LOG_TAG, "Wet bar is found!");
+        wetBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressChanged = 0;
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChanged = progress;
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Log.d(InitialScr.LOG_TAG, "Wet bar is touched!");
+                Toast.makeText(getActivity().getBaseContext(), "wet Bar Progress: " + progressChanged, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        SeekBar dryBar = (SeekBar) view.findViewById(R.id.dryBar);
+        if (dryBar != null)
+            Log.d(InitialScr.LOG_TAG, "dry bar is found!");
+        dryBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressChanged = 0;
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChanged = progress;
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Log.d(InitialScr.LOG_TAG, "dry bar is touched!");
+                Toast.makeText(getActivity().getBaseContext(), "dry Bar Progress: " + progressChanged, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        SeekBar feedbackBar = (SeekBar) view.findViewById(R.id.feedbackBar);
+        if (feedbackBar != null)
+            Log.d(InitialScr.LOG_TAG, "Feedback bar is found!");
+        feedbackBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressChanged = 0;
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChanged = progress;
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Log.d(InitialScr.LOG_TAG, "Feedback bar is touched!");
+                Toast.makeText(getActivity().getBaseContext(), "Feedback Bar Progress: " + progressChanged, Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
         return view;
     }
